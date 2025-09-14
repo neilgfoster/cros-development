892c81cc42abb6e074536e6d387b5c8b8863cf2a
Neil Foster <1370457+neilgfoster@users.noreply.github.com>
2025-09-14 21:29:38 +0100

    Refactor VS Code setup script to install dependencies and add repository source (#5)

--- .setup/scripts/001-vscode.sh
+++ .setup/scripts/001-vscode.sh
@@ -3,12 +3,27 @@
 # Error handling
 set -e
 
-# Update repository and install essential packages
-sudo apt update -y
+# Install dependencies for VS Code repo
 sudo apt install -y \
-  git \
-  git-lfs
-sudo apt autoremove -y
+  wget \
+  gpg
 
-# Initialize git-lfs
-git lfs install
+# Import Microsoft GPG key
+wget -qO- https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > microsoft.gpg
+sudo install -D -o root -g root -m 644 microsoft.gpg /usr/share/keyrings/microsoft.gpg
+rm -f microsoft.gpg
+
+# Add reference to upstream repository
+sudo tee /etc/apt/sources.list.d/vscode.sources > /dev/null <<_EOF_
+Types: deb
+URIs: https://packages.microsoft.com/repos/code
+Suites: stable
+Components: main
+Architectures: amd64,arm64,armhf
+Signed-By: /usr/share/keyrings/microsoft.gpg
+_EOF_
+
+# Install VS Code
+sudo apt install -y apt-transport-https
+sudo apt update -y
+sudo apt install -y code
